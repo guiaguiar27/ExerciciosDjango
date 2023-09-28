b64a03352482fe8d9f40d43ab9c184cfc2045b74
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2023-09-28 17:33:34 -0300

    project

--- asynchronous-views/testproject/testproject/views.py
+++ asynchronous-views/testproject/testproject/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts immport render  
+from django.shortcuts import render  
 import requests, time 
 
 def home_view(request): 
-    start_time = timer.time() 
+    start_time = time.time() 
     data = [] 
-    url_list = ['https://swapi.dev/api/people/1/', 'https://swapi.dev/api/starships/9/'] 
+    url_list = ['https://swapi.dev/api/people', 'https://swapi.dev/api/starships'] 
     for url in url_list: 
-        data.append(request.get(url).json()) 
-    final_time = timer.time() - start_time 
+        data.append(requests.get(url).json()) 
+    final_time = time.time() - start_time 
+    print(time)
     return render(request, 'home.html', {'people':data[0], 'starships':data[1]})
