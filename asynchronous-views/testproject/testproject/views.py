393fdb5c7024dca099cb5f25bfd7d071eed0b9e3
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2023-09-28 18:11:33 -0300

    commit

--- asynchronous-views/testproject/testproject/views.py
+++ asynchronous-views/testproject/testproject/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render  
-import requests, time 
-
+import requests, time, aiohttp, asyncio   
+from .utils import fetch
+async def home_view(request): 
+    start_time = time.time() 
+    url_list = ['http://swapi.dev/api/people','https://swapi.dev/api/starship'] 
+    async with aiohttp.ClientSession() as Client: 
+        tasks = [] 
+        for url in url_list: 
+            task = asyncio.ensure_future(fetch(Client,url)) 
+            tasks.append(task) 
+        results =  await asyncio.gather(*tasks) 
+    final_time = time.time() - start_time 
+    print(final_time) 
+    return render(request, 'home.html', {'people':results[0], 'starship':results[1]})
+'''
 def home_view(request): 
     start_time = time.time() 
     data = [] 
@@ -8,5 +21,6 @@ def home_view(request):
     for url in url_list: 
         data.append(requests.get(url).json()) 
     final_time = time.time() - start_time 
-    print(time)
-    return render(request, 'home.html', {'people':data[0], 'starships':data[1]})
+    print(final_time)
+    return render(request, 'home.html', {'people':data[0], 'starships':data[1]}) 
+    '''
